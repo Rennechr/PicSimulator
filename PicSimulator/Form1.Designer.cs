63d16b6f5ccabc261ca8d5b8b32d1550c0ff6ce6
Chris <christianrenner6@gmail.com>
2021-06-07 16:51:56 +0200

    update

--- PicSimulator/Form1.Designer.cs
+++ PicSimulator/Form1.Designer.cs
@@ -879,7 +879,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(779, 542);
+            this.ClientSize = new System.Drawing.Size(1389, 895);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
